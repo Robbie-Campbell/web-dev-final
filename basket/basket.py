0c89f55c2f6c5ffb01ba3c021182308a3f6d75fb
robbie <robbielcampbell@hotmail.com>
2021-06-06 15:18:24 +0100

    :shipit: basket sorted :shipit:

--- basket/basket.py
+++ basket/basket.py
@@ -1,6 +1,9 @@
-
+from recipe.models import Recipe
+from decimal import Decimal
 
 class Basket():
+
+    # Create the basket for a new session or get currect session data
     def __init__(self, request):
         self.session = request.session
         basket = self.session.get('skey')
@@ -8,14 +11,49 @@ class Basket():
             basket = self.session['skey'] = {}
         self.basket = basket
 
+    # Add a recipe to the basket
     def add(self, recipe, qty):
         recipe_id = recipe.id
         if recipe_id in self.basket:
             self.basket[recipe_id]['qty'] = qty
         else:
-            self.basket[recipe_id] = {'price': int(recipe.price), 'qty': qty}
+            self.basket[recipe_id] = {'price': str(recipe.price), 'qty': qty}
         
         self.session.modified = True
 
+    # Get the number of items in the basket
     def __len__(self):
         return sum(item['qty'] for item in self.basket.values())
+
+    # Return all of the items in the basket to be looped through
+    def __iter__(self):
+        recipe_ids = self.basket.keys()
+        recipes = Recipe.objects.filter(id__in=recipe_ids)
+        basket = self.basket.copy()
+
+        for recipe in recipes:
+            basket[str(recipe.id)]['recipe'] = recipe
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = Decimal(item['price']) * item['qty']
+            yield item
+
+    def get_total_price(self):
+        return sum(Decimal(Decimal(item['price'])) * item['qty'] for item in self.basket.values())
+
+    def update(self, recipe, qty):
+        recipe_id = str(recipe)
+        if recipe_id in self.basket:
+            self.basket[recipe_id]['qty'] = qty
+        self.save()
+
+    def delete(self, recipe):
+        recipe_id = str(recipe)
+        if recipe_id in self.basket:
+            del self.basket[recipe_id]
+            print(recipe_id)
+            self.save()
+
+    def save(self):
+        self.session.modified = True
--- basket/views.py
+++ basket/views.py
@@ -4,7 +4,8 @@ from recipe.models import Recipe
 from django.http import JsonResponse
 
 def basket_summary(request):
-    return render(request, 'store/basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'store/basket/summary.html', {"basket": basket})
 
 def basket_add(request):
     basket = Basket(request)
@@ -15,4 +16,28 @@ def basket_add(request):
         basket.add(recipe=recipe, qty=recipe_qty)
         basketqty = basket.__len__()
         response = JsonResponse({'qty': basketqty})
+        return response
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        recipe_id = int(request.POST.get('recipeid'))
+        basket.delete(recipe=recipe_id)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
+
+
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        recipe_id = int(request.POST.get('recipeid'))
+        recipe_qty = int(request.POST.get('recipeqty'))
+        basket.update(recipe=recipe_id, qty=recipe_qty)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
         return response
