ad982bb4b46b7de8360d5bfa4072245064c1d96e
robbie <robbielcampbell@hotmail.com>
2021-06-08 18:11:20 +0100

    :rocket: big changes :rocket:

--- account/forms.py
+++ account/forms.py
@@ -1,10 +1,21 @@
 from django import forms
 from .models import UserBase
+from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm, SetPasswordForm
 
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'login-username'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'id': 'login-pwd',
+        }
+    ))
 
 class RegistrationForm(forms.ModelForm):
     username = forms.CharField(label="Enter Username", min_length=4, max_length=50, help_text="Required")
-    email = forms.EmailField(label="Enter Email", min_length=4, max_length=100, error_messages={'required': 'Please enter an Email'})
+    email = forms.EmailField(label="Enter Email", min_length=4, max_length=100, required=False, error_messages={'required': 'Please enter an Email'})
     password1 = forms.CharField(label="Enter Password", widget=forms.PasswordInput)
     password2 = forms.CharField(label="Repeat Password", widget=forms.PasswordInput)
 
@@ -36,3 +47,73 @@ class RegistrationForm(forms.ModelForm):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
+
+class UserEditForm(forms.ModelForm):
+    email = forms.EmailField(
+        label='Account Email (Cannot be changed)', max_length=100, disabled=True, widget=forms.TextInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Email', 'id':'form-email', 'readonly': 'readonly'}))
+
+    username = forms.CharField(
+        label='Username', max_length=100, disabled=True, widget=forms.TextInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Username', 'id':'form-username', 'readonly': 'readonly'}))
+
+    first_name = forms.CharField(
+        label='First Name', max_length=100, required=False, widget=forms.TextInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'First Name', 'id':'form-firstname'}))
+
+    last_name = forms.CharField(
+        label='Last Name', max_length=100, required=False, widget=forms.TextInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Last Name', 'id':'form-lastname'}))
+
+    address_line_1 = forms.CharField(
+        label='Address Line 1', max_length=100, required=False, widget=forms.TextInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Add Ln 1', 'id':'form-add1'}))
+
+    phone_number = forms.CharField(
+        label='Phone Number', max_length=100, required=False, widget=forms.TextInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Phone Number', 'id':'form-phone'}))
+
+    address_line_2 = forms.CharField(
+        label='Address Line 2', max_length=100, required=False, widget=forms.TextInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Add Ln 2', 'id':'form-add2'}))
+
+    postcode = forms.CharField(
+        label='Postcode', max_length=100, required=False, widget=forms.TextInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'postcode', 'id':'form-postcode'}))
+
+    town_city = forms.CharField(
+        label='Town / City', max_length=100, required=False, widget=forms.TextInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Town / City', 'id':'form-towncity'}))
+
+    class Meta:
+        model = UserBase
+        fields = ('email', 'username', 'first_name', 'last_name', 'address_line_1', 'address_line_2', 'postcode')
+
+    def __init__(self, *args, **kwargs):
+        super(UserEditForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            self.fields['username'].required = True
+            self.fields['email'].required = True
+
+class PwdResetForm(PasswordResetForm):
+    email = forms.EmailField(
+    max_length=100, widget=forms.TextInput(
+        attrs={'class':'form-control mb-2', 'placeholder':'Email', 'id':'form-email'}))
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        u = UserBase.objects.get(email=email)
+        if not u:
+            raise forms.ValidationError(
+                "We cannot find that email address."
+            )
+        return email
+
+class PwdResetConfirmForm(SetPasswordForm):
+    new_password1 = forms.CharField(
+        label='New Password', max_length=100, widget=forms.PasswordInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'New Password', 'id':'form-new-pass1'}))
+
+    new_password2 = forms.CharField(
+        label='Repeat Password', max_length=100, widget=forms.PasswordInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Repeat Password', 'id':'form-new-pass2'}))
--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
-from .forms import RegistrationForm
+from .forms import RegistrationForm, UserEditForm
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
@@ -14,8 +14,23 @@ from .models import UserBase
 def dashboard(request):
     return render(request, 'account/user/dashboard.html', {'section': 'profile'})
 
-def register_success(request):
-    return render(request, 'account/registration/register_success.html', {'section': 'profile'})
+@login_required
+def edit_profile(request):
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        if user_form.is_valid():
+            user_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+    return render(request, 'account/user/edit_profile.html', {'form': user_form})
+
+@login_required
+def delete_profile(request):
+    user = UserBase.objects.get(username=request.user)
+    user.is_active = False
+    user.save()
+    logout(request)
+    return redirect('account:delete_confirm')
 
 def register(request):
     if request.user.is_authenticated:
